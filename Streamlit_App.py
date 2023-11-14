bdd0282b20c9cf632fbcf3c2a049a5e96f202c2f
AbhijithNidanakavi <91921508+AbhijithNidanakavi@users.noreply.github.com>
2023-11-14 10:51:21 -0800

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -1,1 +1,36 @@
 
+import streamlit as st
+import pickle 
+import numpy as np 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+
+def load_tfidf():
+    tfidf = pickle.load(open("tf_idf.pkt", "rb"))
+    return tfidf
+
+def load_model():
+    nb_model = pickle.load(open("model.pkt", "rb"))
+    return nb_model
+
+def news_prediction(text):
+    tfidf = load_tfidf()
+    text_tfidf = tfidf.transform([text]).toarray()
+    nb_model = load_model()
+    prediction = nb_model.predict(text_tfidf)
+    class_name = "Propagandistic" if prediction == 1 else "Non-Propagandistic"
+    return class_name
+
+st.title("Truth Seeker App")
+st.image("1.jpg")
+
+st.subheader("Input your text")
+
+text_input = st.text_input("Enter your text")
+
+if text_input is not None:
+    if st.button("Analyse"):
+        result = news_prediction(text_input)
+        st.subheader("Result:")
+        st.info("The article is "+ result + ".")
